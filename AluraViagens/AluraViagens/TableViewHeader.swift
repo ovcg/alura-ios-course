508db4f5e8bc6fe7b8a4d631241f3c547073cee7
otavio <17763111+ovcg@users.noreply.github.com>
2025-04-25 15:19:02 -0300

    add alura viagens project

---
+++ AluraViagens/AluraViagens/TableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Otávio Gomes on 25/04/25.
+//
+
+import UIKit
+
+class TableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
