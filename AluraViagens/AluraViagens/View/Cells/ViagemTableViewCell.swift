e62bbd1e23c2503f20c1d8f10dffec596d981fbe
otavio <17763111+ovcg@users.noreply.github.com>
2025-04-28 17:00:43 -0300

    add auto layout for ipad

---
+++ AluraViagens/AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Otávio Gomes on 28/04/25.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ AluraViagens/AluraViagens/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViagemViewModel.swift
+//  AluraViagens
+//
+//  Created by Otávio Gomes on 28/04/25.
+//
+
+import Foundation
