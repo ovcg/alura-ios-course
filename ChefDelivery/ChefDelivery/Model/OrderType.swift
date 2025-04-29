a7ec9a2e9a9d7e32cc21ee0be411b16433817a13
otavio <17763111+ovcg@users.noreply.github.com>
2025-04-29 15:22:24 -0300

    add images and gridview

---
+++ ChefDelivery/ChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  ChefDelivery
+//
+//  Created by Otávio Gomes on 29/04/25.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
